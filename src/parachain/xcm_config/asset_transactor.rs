8231b7f599305d046267f53f59043f9bc32f646b
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2024-04-22 10:36:13 +0200

    feat(location-converter): use HashedDescription and standardize

--- src/parachain/xcm_config/asset_transactor.rs
+++ src/parachain/xcm_config/asset_transactor.rs
@@ -2,36 +2,28 @@ pub use sandbox::*;
 
 #[cfg(feature = "start")]
 mod sandbox {
-    pub type SovereignAccountOf = ();
     pub type AssetTransactor = ();
 }
 
 #[cfg(feature = "example")]
 mod sandbox {
-    use polkadot_parachain_primitives::primitives::Sibling;
     use xcm::latest::prelude::*;
     use xcm_builder::{
-        AccountId32Aliases, ConvertedConcreteId, FungibleAdapter, IsConcrete, NoChecking,
-        NonFungiblesAdapter, ParentIsPreset, SiblingParachainConvertsVia,
+        ConvertedConcreteId, FungibleAdapter, IsConcrete, NoChecking,
+        NonFungiblesAdapter,
     };
     use xcm_executor::traits::JustTry;
 
     use crate::parachain::{
-        AccountId, Balances, ForeignUniques, KsmLocation, LocationToAccountId, RelayNetwork,
+        AccountId, Balances, ForeignUniques, KsmLocation, LocationToAccountId,
     };
-
-    pub type SovereignAccountOf = (
-        SiblingParachainConvertsVia<Sibling, AccountId>,
-        AccountId32Aliases<RelayNetwork, AccountId>,
-        ParentIsPreset<AccountId>,
-    );
 
     type LocalAssetTransactor = (
         FungibleAdapter<Balances, IsConcrete<KsmLocation>, LocationToAccountId, AccountId, ()>,
         NonFungiblesAdapter<
             ForeignUniques,
             ConvertedConcreteId<Location, AssetInstance, JustTry, JustTry>,
-            SovereignAccountOf,
+            LocationToAccountId,
             AccountId,
             NoChecking,
             (),
