5e8aa326bc84c809b2fab939bc435a09ff6c91ef
Shawn Tabrizi <shawntabrizi@gmail.com>
2024-05-02 17:54:14 -0400

    better test script

--- test_all.sh
+++ test_all.sh
@@ -1,2 +1,22 @@
-cargo test --features start --no-default-features
-cargo test --features example --no-default-features
+
+#!/bin/bash
+
+# List of cargo features to be tested
+features=(
+    "start"
+    "example"
+)
+
+# Run cargo test commands
+for feature in "${features[@]}"; do
+    cargo test --features $feature --no-default-features
+
+    # Check the exit status of the previous command
+    if [ $? -ne 0 ]; then
+        echo "❌ Tests failed: $feature"
+		echo "Exit early"
+        exit 1
+    fi
+done
+
+echo "✅︎ All tests pass."
--- test_fundamentals.sh
+++ test_fundamentals.sh
@@ -1,5 +1,25 @@
-cargo test --features fundamentals,location --no-default-features
-cargo test --features fundamentals,asset --no-default-features
-cargo test --features fundamentals,instruction --no-default-features
-cargo test --features fundamentals,xcm_executor --no-default-features
-cargo test --features fundamentals,pallet_xcm --no-default-features
+
+#!/bin/bash
+
+# List of cargo features to be tested
+features=(
+    "fundamentals,location"
+    "fundamentals,asset"
+    "fundamentals,instruction"
+	"fundamentals,xcm_executor"
+	"fundamentals,pallet_xcm"
+)
+
+# Run cargo test commands
+for feature in "${features[@]}"; do
+    cargo test --features $feature --no-default-features
+
+    # Check the exit status of the previous command
+    if [ $? -ne 0 ]; then
+        echo "❌ Tests failed: $feature"
+		echo "Exit early"
+        exit 1
+    fi
+done
+
+echo "✅︎ All tests pass."
