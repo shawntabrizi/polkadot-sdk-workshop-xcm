c3157c1ce95e8f8a48dc6d5c74c80f26ed8acd31
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2024-05-13 13:19:30 +0200

    feat(fundamentals-instruction): introduce some basic instructions

--- src/tests/fundamentals/instruction.rs
+++ src/tests/fundamentals/instruction.rs
@@ -2,8 +2,42 @@ use crate::fundamentals::instruction::*;
 use xcm::latest::prelude::*;
 
 #[test]
-fn test_clear_origin_message() {
+fn clear_origin_message_correct() {
     let message = clear_origin_message();
 
-    assert_eq!(message, Xcm(vec![ClearOrigin,]));
+    assert_eq!(message, Xcm(vec![ClearOrigin]));
+}
+
+#[test]
+fn withdraw_asset_correct() {
+    let message = withdraw_asset();
+
+    assert_eq!(message, Xcm(vec![WithdrawAsset((Parent, 100u128).into())]));
+}
+
+#[test]
+fn withdraw_and_deposit_correct() {
+    let message = withdraw_and_deposit();
+
+    assert_eq!(
+        message,
+        Xcm(vec![
+            WithdrawAsset((Parent, 100u128).into()),
+            DepositAsset { assets: All.into(), beneficiary: AccountId32 { id: crate::ALICE.into(), network: None }.into() }
+        ])
+    );
+}
+
+#[test]
+fn withdraw_and_deposit_paying_fees_correct() {
+    let message = withdraw_and_deposit_paying_fees();
+
+    assert_eq!(
+        message,
+        Xcm(vec![
+            WithdrawAsset((Parent, 100u128).into()),
+            BuyExecution { fees: (Parent, 10u128).into(), weight_limit: Unlimited.into() },
+            DepositAsset { assets: All.into(), beneficiary: AccountId32 { id: crate::ALICE.into(), network: None }.into() }
+        ])
+    );
 }
