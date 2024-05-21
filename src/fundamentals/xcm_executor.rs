cccc3a5e1282210674b77f3d3628202227cd3d00
Shawn Tabrizi <shawntabrizi@gmail.com>
2024-05-20 23:42:17 -0400

    key takeaways for xcm executor

--- src/fundamentals/xcm_executor.rs
+++ src/fundamentals/xcm_executor.rs
@@ -2,6 +2,14 @@
 //!
 //! Create your own executor for XCM.
 
+// TODO: Key Takeaways
+// - Match statement to process instructions
+// - executor state (origin, holding, etc...)
+// - Assets in Holding, and all its behaviors
+// - How XCM Config can change logic in the executor
+// - Transactional
+// - composability of instructions
+
 use frame_support::{
 	dispatch::{GetDispatchInfo, PostDispatchInfo},
 	Parameter,
