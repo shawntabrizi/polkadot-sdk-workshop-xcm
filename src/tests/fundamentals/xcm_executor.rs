803a2591c4348ef67be2d2d6828b744f8484d186
Shawn Tabrizi <shawntabrizi@gmail.com>
2024-05-20 21:22:54 -0400

    copy paste some xcm executor stuff to make it work

--- src/tests/fundamentals/xcm_executor.rs
+++ src/tests/fundamentals/xcm_executor.rs
@@ -1,12 +1,38 @@
-use crate::{fundamentals::xcm_executor::*, parachain, ParaA};
+use crate::{
+	fundamentals::xcm_executor::*,
+	parachain,
+	parachain::{
+		constants::KsmLocation, location_converter::LocationConverter, AccountId, Balances,
+		ForeignUniques,
+	},
+	ParaA,
+};
 use frame_support::assert_ok;
 use xcm::latest::prelude::*;
+use xcm_builder::{
+	ConvertedConcreteId, FrameTransactionalProcessor, FungibleAdapter, IsConcrete, NoChecking,
+	NonFungiblesAdapter,
+};
+use xcm_executor::traits::JustTry;
 use xcm_simulator::TestExt;
+
+type TestAssetTransactor = (
+	FungibleAdapter<Balances, IsConcrete<KsmLocation>, LocationConverter, AccountId, ()>,
+	NonFungiblesAdapter<
+		ForeignUniques,
+		ConvertedConcreteId<Location, AssetInstance, JustTry, JustTry>,
+		LocationConverter,
+		AccountId,
+		NoChecking,
+		(),
+	>,
+);
 
 struct Config;
 impl XcmConfig for Config {
 	type RuntimeCall = parachain::RuntimeCall;
-	type AssetTransactor = parachain::asset_transactor::AssetTransactor;
+	type AssetTransactor = TestAssetTransactor;
+	type TransactionalProcessor = FrameTransactionalProcessor;
 	type Barrier = ();
 }
 
@@ -16,18 +42,19 @@ fn clear_origin_works() {
 }
 
 // TODO FIX
-// #[test]
-// fn withdraw_works() {
-// 	ParaA::execute_with(|| {
-// 		let message = Xcm::<parachain::RuntimeCall>::builder_unsafe()
-// 			.withdraw_asset((Parent, 100u128))
-// 			.build();
-// 		let origin = AccountId32 { id: crate::ALICE.into(), network: None }.into();
-// 		let mut executor = XcmExecutor::<Config>::new(origin);
-// 		assert_ok!(executor.execute(message));
-// 		assert_eq!(executor.holding.fungible.get(&Parent.into()), Some(&100u128));
-// 	});
-// }
+#[test]
+fn withdraw_works() {
+	ParaA::execute_with(|| {
+		let message = Xcm::<parachain::RuntimeCall>::builder_unsafe()
+			.withdraw_asset((Parent, 100u128))
+			.build();
+		let origin: Location = AccountId32 { id: crate::ALICE.into(), network: None }.into();
+
+		let mut executor = XcmExecutor::<Config>::new(origin);
+		assert_ok!(executor.execute(message));
+		assert_eq!(executor.holding.fungible.get(&Parent.into()), Some(&100u128));
+	});
+}
 
 #[test]
 fn buy_execution_works() {
