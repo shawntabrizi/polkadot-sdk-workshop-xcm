60d80419cd28335741447feb530686685fe000a1
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2025-03-30 23:44:25 +0200

    chore: specify para_id in parachain

--- emulator/src/parachain/genesis.rs
+++ emulator/src/parachain/genesis.rs
@@ -3,6 +3,10 @@ use emulated_integration_tests_common::build_genesis_storage;
 
 pub fn genesis() -> Storage {
     let genesis_config = parachain_runtime::RuntimeGenesisConfig {
+        parachain_info: parachain_info::GenesisConfig {
+            parachain_id: 2000.into(),
+            ..Default::default()
+        },
         ..Default::default()
     };
 
