da28af90815b0d3d68070439c2e9e8d6594199b1
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2025-03-31 00:05:52 +0200

    fix: add actual parachain instead of submodule

---
+++ parachain/src/benchmarks.rs
@@ -0,0 +1,12 @@
+frame_benchmarking::define_benchmarks!(
+    // Only benchmark the following pallets
+    [frame_system, SystemBench::<Runtime>]
+    [cumulus_pallet_parachain_system, ParachainSystem]
+    [pallet_timestamp, Timestamp]
+    [pallet_balances, Balances]
+    [pallet_sudo, Sudo]
+    [pallet_collator_selection, CollatorSelection]
+    [pallet_session, SessionBench::<Runtime>]
+    [cumulus_pallet_xcmp_queue, XcmpQueue]
+    [pallet_message_queue, MessageQueue]
+);
