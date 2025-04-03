9056b67ae8fa38f16c6027c0f07a8d4d687d9ed0
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2025-04-04 01:44:46 +0200

    remove barrier

--- parachain/src/configs/xcm/barrier.rs
+++ parachain/src/configs/xcm/barrier.rs
@@ -1,3 +1,5 @@
+#![allow(unused_imports)]
+
 use super::RelayLocation;
 
 use frame_support::traits::{EverythingBut, Equals};
@@ -5,5 +7,5 @@ use xcm_builder::{AllowTopLevelPaidExecutionFrom, TakeWeightCredit};
 
 pub type Barrier = (
     TakeWeightCredit,
-    AllowTopLevelPaidExecutionFrom<EverythingBut<Equals<RelayLocation>>>,
+    // TODO: Put the new necessary barrier here. Hint: use the imports :)
 );
