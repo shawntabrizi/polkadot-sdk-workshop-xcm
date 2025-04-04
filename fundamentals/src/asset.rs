60549e62eb834abdb9b9d0c8ac673cfc0ec7b6d4
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2025-04-04 15:26:58 +0200

    fix: change TODOs to todo macro

--- fundamentals/src/asset.rs
+++ fundamentals/src/asset.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 //! # Fundamentals Lesson 2
 //!
 //! All locations in this module are relative to Polkadot parachain 2000.
@@ -44,31 +45,31 @@ const USDT_DECIMALS: u32 = 6;
 // Construct these assets from the perspective of AssetHub (1000).
 parameter_types! {
 	// `Assets` instance that contains no assets.
-	pub EmptyAssets: Assets = // TODO
+	pub EmptyAssets: Assets = todo!();
 	// USDT.
-	pub Usdt: AssetId = // TODO
+	pub Usdt: AssetId = todo!();
 	// The native token of the relay chain, i.e. DOT.
-	pub DotToken: AssetId = // TODO
+	pub DotToken: AssetId = todo!();
 	// 100 USDT.
-	pub OneHundredUsdt: Asset = // TODO
+	pub OneHundredUsdt: Asset = todo!();
 	// Some amount of the native token of the relay chain.
-	pub OneHundredDot: Asset = // TODO
+	pub OneHundredDot: Asset = todo!();
 }
 
 // Non-Fungible Tokens
 parameter_types! {
 	// Location of NFT collection with id 3 inside of the NFT pallet in Polkadot parachain 1000.
-	pub NftLocation: Location = // TODO
+	pub NftLocation: Location = todo!();
 	// The NFT with id 69 inside of that collection.
-	pub Nft: Asset = // TODO
+	pub Nft: Asset = todo!();
 }
 
 // Asset Filters
 parameter_types! {
 	// A filter which will capture all possible assets.
-	pub AllAssetsFilter: AssetFilter = // TODO
+	pub AllAssetsFilter: AssetFilter = todo!();
 	// A filter specific for the DOT Token.
-	pub DotFilter: AssetFilter = // TODO
+	pub DotFilter: AssetFilter = todo!();
 	// A filter specific for USDT.
-	pub UsdtFilter: AssetFilter = // TODO
+	pub UsdtFilter: AssetFilter = todo!();
 }
