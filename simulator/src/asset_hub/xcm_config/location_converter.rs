fa0141803616ff2119c26739b563bdda21cbfa98
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2025-04-04 15:17:50 +0200

    chore: remove simulator

--- simulator/src/asset_hub/xcm_config/location_converter.rs
+++
@@ -1,9 +0,0 @@
-use super::{super::AccountId, constants::RelayNetwork};
-use xcm_builder::{AccountId32Aliases, DescribeAllTerminal, DescribeFamily, HashedDescription};
-
-type LocationToAccountId = (
-	HashedDescription<AccountId, DescribeFamily<DescribeAllTerminal>>,
-	AccountId32Aliases<RelayNetwork, AccountId>,
-);
-
-pub type LocationConverter = LocationToAccountId;
