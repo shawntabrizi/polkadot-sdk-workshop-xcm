ad2d1cb216c66bf2afac9cc8ef3120e0c85ea8a2
Alexander Popiak <alexander.popiak@gmail.com>
2025-09-07 08:24:46 +0800

    format barrier file

--- parachain/src/configs/xcm/barrier.rs
+++ parachain/src/configs/xcm/barrier.rs
@@ -1,25 +1,23 @@
 #![allow(unused_imports)]
 
-use frame_support::traits::Everything;
-use xcm_builder::{AllowTopLevelPaidExecutionFrom, TakeWeightCredit};
-use xcm_executor::traits::ShouldExecute;
+use frame_support::traits::{Everything, ProcessMessageError};
 use xcm::prelude::*;
-use xcm_executor::traits::Properties;
-use frame_support::traits::ProcessMessageError;
+use xcm_builder::{AllowTopLevelPaidExecutionFrom, TakeWeightCredit};
+use xcm_executor::traits::{Properties, ShouldExecute};
 
 pub struct AllowAll;
 impl ShouldExecute for AllowAll {
-    fn should_execute<Call>(
-        _origin: &Location,
-        _instructions: &mut [Instruction<Call>],
-        _max_weight: Weight,
-        _properties: &mut Properties
-    ) -> Result<(), ProcessMessageError> {
-        Ok(())
-    }
+	fn should_execute<Call>(
+		_origin: &Location,
+		_instructions: &mut [Instruction<Call>],
+		_max_weight: Weight,
+		_properties: &mut Properties,
+	) -> Result<(), ProcessMessageError> {
+		Ok(())
+	}
 }
 
 pub type Barrier = (
-    TakeWeightCredit,
-    AllowAll, // TODO replace with correct barrier
+	TakeWeightCredit,
+	AllowAll, // TODO replace with correct barrier
 );
