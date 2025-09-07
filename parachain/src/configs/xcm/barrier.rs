5d5a4ad9150e6c4b46d3840af341c2c1ef07a217
Alexander Popiak <alexander.popiak@gmail.com>
2025-09-06 21:38:37 -0600

    improve barrier exercise (#18)
    * add faulty barrier to make the test fail at start of exercise
    
    * format barrier file

--- parachain/src/configs/xcm/barrier.rs
+++ parachain/src/configs/xcm/barrier.rs
@@ -1,9 +1,23 @@
 #![allow(unused_imports)]
 
-use frame_support::traits::Everything;
+use frame_support::traits::{Everything, ProcessMessageError};
+use xcm::prelude::*;
 use xcm_builder::{AllowTopLevelPaidExecutionFrom, TakeWeightCredit};
+use xcm_executor::traits::{Properties, ShouldExecute};
+
+pub struct AllowAll;
+impl ShouldExecute for AllowAll {
+	fn should_execute<Call>(
+		_origin: &Location,
+		_instructions: &mut [Instruction<Call>],
+		_max_weight: Weight,
+		_properties: &mut Properties,
+	) -> Result<(), ProcessMessageError> {
+		Ok(())
+	}
+}
 
 pub type Barrier = (
-    TakeWeightCredit,
-    // TODO: Put the new necessary barrier here. Hint: use the imports :)
+	TakeWeightCredit,
+	AllowAll, // TODO replace with correct barrier
 );
